f128e6cbc5c36349967b6128a3b8487e847537c7
deancox9 <39554891+deancox9@users.noreply.github.com>
2018-05-23 13:25:41 +0100

    Add files via upload

---
+++ MGRE.ETL.Export/ETLCreate.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using MGRE.ETL.Common;
+using MGRE.ETL.Contracts;
+using MGRE.ETL.Application.Data;
+using MGRE.ETL.Email;
+
+namespace MGRE.ETL.Export
+{
+    #region .Net Class Documentation
+    /// <summary>
+    /// Class with methods for creating ETL files
+    /// </summary>
+    /// <remarks> </remarks> 
+    /// <history>
+    ///	  <revision by="deanc" ref="24063" date="2013-07-30">created</revision>
+    /// </history>
+    #endregion
+    public class ETLCreate
+    {
+        public ETLCreate() { }
+
+        public bool CreateETLFiles(ETLExportDefinition exportDefinition, string exportFileLocation, string userName)
+        {
+            ETLData data = new ETLData(exportDefinition.ApplicationDatabase);
+            DateTime startDateTime = DateTime.Now;
+            try
+            {
+                foreach (ETLExportTablesDefinition table in exportDefinition.TableDefinitions)
+                {
+                    //Retrieve DataSet for particular ETL Table
+                    DataSet ds = new DataSet();
+                    ds = data.GetETLData(table.ProcedureName, table.TableName);
+
+                    if (ds != null)
+                    {
+                        //Save all files into temp area (in case one or more files fail)
+                        string exportETLFileLocation = exportFileLocation + @"\" +
+                                                    exportDefinition.ExportSubDirectory + @"\Temp\" +
+                                                    table.ETLFileName.Replace(".csv", DateTimeSuffix());
+
+                        CreateCSVFile(ds, exportETLFileLocation, table.ETLHeaderName);
+                    }
+                }
+
+                
+                //Move all files from temp area into pick-up area
+                foreach (string etlFile in Directory.GetFiles(exportFileLocation + @"\" +
+                                                    exportDefinition.ExportSubDirectory + @"\Temp\"))
+                {
+                    System.IO.File.Move(etlFile, etlFile.Replace(@"Temp\", ""));
+                }
+
+                ////Now mark ETL as processed! - update _Status column of record
+                //data.UpdateETLDataStatus(exportDefinition.StatusProcedureName, (int)Common.Enums.ETLStatus.Success);
+                //Status update is done in Email method below!
+
+                //Email notification that file has been created....
+                Email.EmailETL email = new Email.EmailETL();
+                email.Email(exportDefinition, "");
+
+                //Record Import History
+                ETLData hubData = new ETLData("HUB");
+                hubData.RecordHistory(exportDefinition.ETLExportGUID, userName, startDateTime, true);
+
+                
+            }
+            catch (Exception ex)
+            {
+                MGRELog.Write(ex);
+
+                //Now mark ETL as failed! - update _Status column of record
+                data.UpdateETLDataStatus(exportDefinition.StatusProcedureName, (int)Common.Enums.ETLStatus.Failed);
+
+                //Record Import History
+                ETLData hubData = new ETLData("HUB");
+                hubData.RecordHistory(exportDefinition.ETLExportGUID, userName, startDateTime, false);
+
+                throw ex;
+            }
+
+            return true;
+        }
+
+        public bool CreateETLFilesForEmail(ETLExportDefinition exportDefinition, string exportFileLocation, string userName)
+        {
+            ETLData data = new ETLData(exportDefinition.ApplicationDatabase);
+            DateTime startDateTime = DateTime.Now;
+            try
+            {
+                foreach (ETLExportTablesDefinition table in exportDefinition.TableDefinitions)
+                {
+                    //Retrieve DataSet for particular ETL Table
+                    DataSet ds = new DataSet();
+                    ds = data.GetETLData(table.ProcedureName, table.TableName);
+
+                    if (ds != null)
+                    {
+                        //Save all files into temp area (in case one or more files fail)
+                        string exportETLFileLocation = exportFileLocation + @"\" +
+                                                    exportDefinition.ExportSubDirectory + @"\Temp\" +
+                                                    table.ETLFileName.Replace(".csv", DateTimeSuffix());
+
+                        CreateCSVFile(ds, exportETLFileLocation, table.ETLHeaderName);
+                    }
+                }
+
+                //Email ETL File
+                Email.EmailETL email = new Email.EmailETL();
+                email.Email(exportDefinition, exportFileLocation + @"\" + exportDefinition.ExportSubDirectory + @"\Temp\");
+              
+                //Delete files from temp area
+                foreach (string etlFile in Directory.GetFiles(exportFileLocation + @"\" +
+                                                    exportDefinition.ExportSubDirectory + @"\Temp\"))
+                {
+                    System.IO.File.Delete(etlFile);
+                }
+
+                //Record Import History
+                ETLData hubData = new ETLData("HUB");
+                hubData.RecordHistory(exportDefinition.ETLExportGUID, userName, startDateTime, true);
+
+            }
+            catch (Exception ex)
+            {
+                MGRELog.Write(ex);
+
+                //Delete files from temp area
+                foreach (string etlFile in Directory.GetFiles(exportFileLocation + @"\" +
+                                                    exportDefinition.ExportSubDirectory + @"\Temp\"))
+                {
+                    System.IO.File.Delete(etlFile);
+                }
+
+                //Now mark ETL as failed! - update _Status column of record
+                data.UpdateETLDataStatus(exportDefinition.StatusProcedureName, (int)Common.Enums.ETLStatus.Failed);
+
+                //Record Import History
+                ETLData hubData = new ETLData("HUB");
+                hubData.RecordHistory(exportDefinition.ETLExportGUID, userName, startDateTime, false);
+
+                throw ex;
+            }
+
+            return true;
+        }
+
+        private static void CreateCSVFile(DataSet data,
+                                    string csvFilename,
+                                    string etlHeader)
+        {
+
+            //Create a StreamWriter object to open and write contents
+            //of the DataGridView columns and rows.
+            StreamWriter sr = File.CreateText(csvFilename);
+
+            //Create a variable to hold the delimiter type 
+            //(i.e., TAB or comma or whatever you choose)
+            //The default for this procedure is a comma (",").
+            string delimiter = ",";
+
+            //Create a variable that holds the total number of columns
+            //in the DataSet.
+            int columnCount = data.Tables[0].Columns.Count - 1;
+
+            if (etlHeader.Length != 0)
+            {
+                sr.WriteLine(etlHeader);
+            }
+
+            //Create a variable to hold the row data
+            string rowData = "";
+
+            //Interate through each column and get/write the column name.
+            for (int i = 0; i <= columnCount; i++)
+            {
+                //Exclude any columns that are prefixed _
+                //These columns are not part of the ETL schema and are used by the upstream application (e.g. _Status is used to marke the status of each individual ETL row)
+                if (data.Tables[0].Columns[i].ColumnName.StartsWith("_") == false)
+                {
+                    //The Replace function will remove the delimiter
+                    //from the field data if found.
+                    rowData += data.Tables[0].Columns[i].ColumnName.Replace(delimiter, "") + (i < columnCount ? delimiter : "");
+                }
+            }
+
+            //Write the column header data to the CSV file.
+            sr.WriteLine(rowData);
+
+            //Now collect data for each row and write to the CSV file.
+            //Loop through each row in the DataGridView.
+            foreach (DataRow row in data.Tables[0].Rows)
+            {
+
+                //Reset the value of the strRowData variable
+                rowData = "";
+
+                for (int j = 0; j <= columnCount; j++)
+                {
+                    if (data.Tables[0].Columns[j].ColumnName.StartsWith("_") == false)
+                    {
+                        //The IIf statement will not put a delimiter after the 
+                        //last value added.
+
+                        //The Replace function will remove the delimiter
+                        //from the field data if found.
+                        if (row[j].ToString() != "")
+                        {
+                            rowData += row[j].ToString().Replace(delimiter, "") + (j < columnCount ? delimiter : "");
+                        }
+                        else
+                        {
+                            rowData += (j < columnCount ? delimiter : "");
+                        }
+
+
+                    }
+                }
+
+                //Write the row data to the CSV file.
+                sr.WriteLine(rowData);
+
+            }
+
+            //Close the StreamWriter object.
+            sr.Close();
+
+        }
+
+        private string DateTimeSuffix()
+        {
+            return DateTime.Now.ToString("yyyyMMddHHmmss") + ".csv";
+        }
+
+    }
+}
---
+++ MGRE.ETL.Export/ETLData.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Transactions;
+using System.Linq;
+using System.Text;
+
+using MGRE.ETL.Application.Data;
+using MGRE.ETL.Common;
+
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using Microsoft.Practices.EnterpriseLibrary.ExceptionHandling;
+
+namespace MGRE.ETL.Export
+{
+    public class ETLData : BaseData
+    {
+        #region Constructors
+        public ETLData(string applicationDatabase)
+            : base(applicationDatabase)
+        {
+            MGRELog.Write("Connected to " + applicationDatabase + " database", "HubData.Constructor");
+
+        }
+
+        #endregion Constructors
+
+        public DataSet GetETLData(string storedProc, string tableName)
+        {
+            DataSet ds = new DataSet();
+
+            base.LoadDataSet(storedProc, ds, new string[] { tableName });
+
+
+            // Always create a file even if there is no data
+            return ds;
+
+            //if (ds.Tables[0].Rows.Count > 0)
+            //{
+            //    return ds;
+            //}
+            //else
+            //{
+            //    return null;
+            //}
+        }
+
+        public int UpdateETLDataStatus(string storedProc, int status)
+        {
+            return base.ExecuteStoredProcAsNonQuery(storedProc, status);
+        }
+
+        public int RecordHistory(Guid exportGUID, string userName, DateTime startDateTime, bool success)
+        {
+            int rowsAffected = 0;
+
+            try
+            {
+                    // Submit the DataSet, capturing the number of rows that were affected.  
+                    // Note that UpdateBehavior must be set as Standard.  This will
+                    // ensure that all updates are rolled back on failure.
+                rowsAffected = db.ExecuteNonQuery("usp_ETL_InsertExportHistory", exportGUID, startDateTime, userName, DateTime.Now, (success ? 1 : 0));
+
+            }
+            catch (DBConcurrencyException dbcEx)
+            {
+                bool rethrow = ExceptionPolicy.HandleException(dbcEx, "Data Logic Exception Policy");
+                if (rethrow) throw;
+            }
+            catch (Exception ex)
+            {
+                bool rethrow = ExceptionPolicy.HandleException(ex, "Data Exception Policy");
+                if (rethrow) throw;
+            }
+
+            return rowsAffected;
+
+        }
+    }
+}
