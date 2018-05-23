8111a4f76308f02e3494297bb3b6094214cc153f
deancox9 <39554891+deancox9@users.noreply.github.com>
2018-05-23 13:23:53 +0100

    Add files via upload

---
+++ MGRE.ETL.Data.Contracts/IETLDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MGRE.ETL.Contracts;
+
+namespace MGRE.ETL.Data.Contracts
+{
+    public interface IETLDAL
+    {
+        List<ETL.Contracts.ETLExportDefinition> GetETLExports();
+        ETL.Contracts.ETLExportDefinition GetETLExportByExportName(string exportName);
+        ETL.Contracts.ETLExportDefinition GetETLExportByExportGUID(Guid exportGUID);
+
+        ETL.Contracts.ETLExportConfiguration GetETLExportConfiguration();
+
+        ETL.Contracts.ETLImportConfiguration GetETLImportConfiguration();
+
+        ETL.Contracts.ETLImportDefinition GetETLImportByImportName(string importName);
+        ETL.Contracts.ETLImportDefinition GetETLImportByImportGUID(Guid importGUID);
+
+        ETL.Contracts.DualAppShortcutSetting GetDualAppSettings(int appID);
+        List<ETL.Contracts.vw_FundsAndProperties> GetFundsAndProperties();
+    }
+}
